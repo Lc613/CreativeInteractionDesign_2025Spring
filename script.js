3c05d24f4bfc0af79a51040bd6e14ddc3e5c591e
Lc613 <3288132604@qq.com>
2025-03-10 16:46:07 +0800

    EXERCISE6-WEEK3

---
+++ script.js
@@ -0,0 +1,20 @@
+  document.write("Hello World!");
+
+  function sayHello(){
+    var response = prompt("What is you name?");
+    alert("Hello"+response+",welcomw to the game")
+  };
+
+  function add(a,b){
+    return a+b;
+  }
+  console.log(add(20,30));
+  console.log(add(134,20094));
+
+  function showTopic(){
+    var x = document.getElementById("demo");
+    x.style.fontSize = "25px";
+    x.style.color = "red";
+  }
+
+  //sayHello();
